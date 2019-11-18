3ccbf664bd1f048a87dc55a10997e18284b860b5
ideal920 <ideal920@sina.com>
2019-11-18 09:31:03 +0800

    初始化修改

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/wyc/springcloud/service/DeptService.java
@@ -0,0 +1,15 @@
+package com.wyc.springcloud.service;
+
+import java.util.List;
+
+import com.wyc.springcloud.entities.Dept;
+
+public interface DeptService{
+	public boolean add(Dept dept);
+	public Dept    get(Long id);
+	public List<Dept> list();
+	
+	
+	public Dept findByName(String name);
+}
+
